72ad9558bebd0107d94e5e0b60511cfa58fa6075
Rajdeep2310 <rajdeepgkp23@gmail.com>
2024-05-26 15:26:51 +0530

    4 commit

--- controllers/quizeController.js
+++ controllers/quizeController.js
@@ -25,7 +25,8 @@ exports.createQuize = async(req, res) =>{
         const _id = new mongoose.Types.ObjectId();
 
         //generating URL
-        const URL = `https://fantastic-starburst-e30271.netlify.app/anonymous/${_id}`;
+       // const URL = `https://fantastic-starburst-e30271.netlify.app/anonymous/${_id}`;
+        const URL = `http://localhost:5173/anonymous/${_id}`;
 
 
         //creating new quize if all data is correct
